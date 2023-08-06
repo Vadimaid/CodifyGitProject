8113d34416b130401cef266990d4e45e7c19ff82
vadim <vadimaid@gmail.com>
2023-08-06 18:24:35 +0600

    Commit for Lesson

--- src/main/java/com/codify/codifygitproject/service/UserService.java
+++ src/main/java/com/codify/codifygitproject/service/UserService.java
@@ -1,2 +1,24 @@
-package com.codify.codifygitproject.service;public class UserService {
+package com.codify.codifygitproject.service;
+
+import com.codify.codifygitproject.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final List<User> users;
+
+    public UserService() {
+        users = new ArrayList<>();
+    }
+
+    public User getUserById(Long id) {
+        if (this.users.isEmpty()) {
+            return null;
+        }
+        return this.users.stream().filter(x -> x.getId().equals(id)).findFirst().get();
+    }
 }
