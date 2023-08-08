0899b2a7b1ce217fd065f2bec027e0f4165351b7
Guziy Alexander <abguzij@gmail.com>
2023-08-08 20:07:24 +0600

    UserServiceImpl (#3)
    Co-authored-by: Александр Гузий <aleksandrguzij@MacBook-Pro-Aleksandr.local>

--- src/main/java/com/codify/codifygitproject/service/UserService.java
+++ src/main/java/com/codify/codifygitproject/service/UserService.java
@@ -1,24 +1,9 @@
 package com.codify.codifygitproject.service;
 
 import com.codify.codifygitproject.entity.User;
-import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
-@Service
-public class UserService {
-
-    private final List<User> users;
-
-    public UserService() {
-        users = new ArrayList<>();
-    }
-
-    public User getUserById(Long id) {
-        if (this.users.isEmpty()) {
-            return null;
-        }
-        return this.users.stream().filter(x -> x.getId().equals(id)).findFirst().get();
-    }
+public interface UserService {
+    Optional<User> getUserById(Long id);
 }
