cea5c03b752be2d17714fc462cea81fbdf6c7055
Ker Tak <89515014507@mail.ru>
2023-08-08 19:33:50 +0600

    Alexander commit

---
+++ src/main/java/com/codify/codifygitproject/controller/Cody5Controller.java
@@ -0,0 +1,16 @@
+package com.codify.codifygitproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/codify_5")
+public class Cody5Controller {
+
+    @GetMapping
+    public String getHello(@RequestParam("name") String name){
+        return "Hello " + name;
+    }
+}
